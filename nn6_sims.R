b789917f69c0a614c0edcd7860848e2b074d38c0
Ben Bolker <bbolker@gmail.com>
2016-07-25 16:55:57 -0400

    sims corrected, started

--- nn6_sims.R
+++ nn6_sims.R
@@ -14,12 +14,12 @@ for (i in 2:ncol(lhs_df)) {
     lhs_df[,i] <- sample(lhs_df[,i])
 }
 lhs_df <- data.frame(run=1:nrow(lhs_df),lhs_df)
-saveRDS(lhs_df,file="lhs_df.rds")
+## saveRDS(lhs_df,file="lhs_df.rds")
 ## pairs(lhs_df,pch=".",gap=0)
  resList <- list()
  for (i in 1:nrow(lhs_df)) {
-     pars <- lhs_df[i,]
-     cat(c(i,pars,"\n")
+     pars <- lhs_df[i,-1]
+     cat(c(i,unlist(pars),"\n"))
          argList <- c(list(nt=1e6,rptfreq=1000,R0_init=4,seed=101,
                            progress=TRUE),
                       as.list(pars))
--- nn7_sims.R
+++ nn7_sims.R
@@ -14,11 +14,11 @@ for (i in 2:ncol(lhs_df)) {
     lhs_df[,i] <- sample(lhs_df[,i])
 }
 ## pairs(lhs_df,pch=".",gap=0)
-
+lhs_df <- data.frame(run=1:nrow(lhs_df),lhs_df)
  resList <- list()
  for (i in 1:nrow(lhs_df)) {
-     pars <- lhs_df[i,]
-     cat(c(i,pars,"\n"))
+     pars <- lhs_df[i,-1]
+     cat(c(i,unlist(pars),"\n"))
          argList <- c(list(nt=1e6,rptfreq=1000,R0_init=4,seed=101,
                            mut_var="gamma",
                            mut_link=multlogit(0.1,0.5),
