b94465133ec3867c13d36c6696a4a6e92f1b7233
Ben Bolker <bbolker@gmail.com>
2016-11-24 18:55:52 -0500

    first pass, next set of sims (cont-time, Sobol)

---
+++ simcode/nn8_sims.R
@@ -0,0 +1,38 @@
+library(pevosim)
+
+library(qrng)
+fn <- "nn_runs_8.rda"
+nsim <- 10
+ranges <- list(mu=c(-3,-1),
+               mut_mean=c(-4,-0.5),
+               mut_sd=c(-1,0.5),
+               N=c(1,3),
+               gamma0=c(2,10))
+npar <- length(ranges)
+pow10 <- function(x) 10^x
+rpow10 <- function(x) round(10^x)
+inv10 <- function(x) 10^(-x)
+trans <- list(pow10,pow10,pow10,rpow10,inv10)
+
+ss <- sobol(nsim,npar)
+for (i in 1:npar) {
+    ss[,i] <- ss[,i]*diff(ranges[[i]])+ranges[[i]][1]
+    ss[,i] <- trans[[i]](ss[,i])
+}
+colnames(ss) <- names(ranges)
+ss_df <- data.frame(run=1:nrow(ss),ss)
+resList <- list()
+for (i in 1:nrow(ss_df)) {
+    pars <- ss_df[i,-1]
+    cat(c(i,unlist(pars),"\n"))
+    argList <- c(list(nt=1e7,R0_init=20,seed=101,
+                      mut_var="beta",
+                      discrete=FALSE,
+                      useCpp=TRUE,
+                      progress=TRUE),
+                 as.list(pars))
+    resList[[i]] <- try(do.call(run_sim,argList))
+    save("ss_df","resList",file=fn)
+}
+
+
