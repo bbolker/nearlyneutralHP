b5c367924067714a5e090078f551b004caf7e7bb
Ben Bolker <bbolker@gmail.com>
2016-12-06 09:27:08 -0500

    update run 13 (oops)

--- simcode/nn13_sims.R
+++ simcode/nn13_sims.R
@@ -2,15 +2,15 @@ library(pevosim)
 library(dplyr)
 library(tidyr)
 
-pardf <- expand.grid(dt=c(1/100,1/200,1/500),
+pardf13 <- expand.grid(dt=c(1/100,1/200,1/500),
                      R0=c(10,1e7),
                      N=c(30,100,300,1000))
 
-resList <- vector(nrow(pardf),mode="list")
-for (i in 1:nrow(pardf)) {
-    print(pardf[i,])
-    resList[[i]] <-
-        try(with(as.list(pardf[i,]),
+resList13 <- vector(nrow(pardf13),mode="list")
+for (i in 1:nrow(pardf13)) {
+    print(pardf13[i,])
+    resList13[[i]] <-
+        try(with(as.list(pardf13[i,]),
              run_sim(nt=1e5,seed=101,dt=dt,
                      mod_init=list(R0=R0),
                      mod_params=list(N=N),
