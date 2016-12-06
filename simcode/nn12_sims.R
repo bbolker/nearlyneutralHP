bc3f94f7bd02bc8076f55baa648792832160601c
Ben Bolker <bbolker@gmail.com>
2016-12-06 00:32:59 -0500

    sim run 12

--- simcode/nn12_sims.R
+++ simcode/nn12_sims.R
@@ -10,25 +10,16 @@ resList <- vector(nrow(pardf),mode="list")
 for (i in 1:nrow(pardf)) {
     print(pardf[i,])
     resList[[i]] <-
-        with(as.list(pardf[i,]),
+        try(with(as.list(pardf[i,]),
              run_sim(nt=1e5,seed=101,dt=dt,
                      mod_init=list(R0=R0),
                      mod_params=list(N=N),
                      progress=TRUE,
-                     hazard=TRUE))
+                     hazard=TRUE)))
+    save("pardf","resList",file="nn12.rda")
 }
-
-base <- run_sim(nt=1e4,seed=101,progress=TRUE,
-            dt=1,hazard=TRUE)
-tstep_dt <- run_sim(nt=1e4,seed=101,progress=TRUE,
-            dt=1/20,hazard=TRUE)
-
-base_hi_dt <- run_sim(nt=1e4,seed=101,progress=TRUE,
-            dt=1,mod_init=list(R0=1e6),hazard=TRUE)
-
-tstep_hi_dt <- run_sim(nt=1e4,seed=101,progress=TRUE,
-            dt=1/20,mod_init=list(R0=1e6),hazard=TRUE)
-
+{
+if (FALSE)  {
 library(ggplot2); theme_set(theme_bw())
 rL <- lme4:::namedList(base,lo_dt,base_hi_dt,tstep_hi_dt)
 x1 <- rL %>% bind_rows(.id="type") %>%
@@ -38,3 +29,4 @@ x1 <- rL %>% bind_rows(.id="type") %>%
 ggplot(x1,aes(time,val,colour=type))+
     geom_line()+facet_wrap(~var,scale="free")+
     scale_y_log10()
+}
